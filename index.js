0249789c42bbe5afe8cdd24ed7532a8847a1a77a
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-11-27 14:02:36 +0600

    added riders role update related apis

--- index.js
+++ index.js
@@ -10,30 +10,27 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./zapshift-web-firebase-adminsdk.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-const firebaseToken = async (req,res,next) =>{
-  const token = req.headers.authorization
-  if(!token){
-    return res.status(401).send({message:'unauthorized access'})
+const firebaseToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
   }
 
-  try{
-    const tokenId = token.split(' ')[1]
-    const decode = await admin.auth().verifyIdToken(tokenId)
-    console.log('decoded in the token',decode)
-    req.decoded_email = decode.email
-  }
-  catch(err){
-
-  }
-  next()
-}
+  try {
+    const tokenId = token.split(" ")[1];
+    const decode = await admin.auth().verifyIdToken(tokenId);
+    console.log("decoded in the token", decode);
+    req.decoded_email = decode.email;
+  } catch (err) {}
+  next();
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g0ilve4.mongodb.net/?appName=Cluster0`;
 
@@ -55,19 +52,19 @@ async function run() {
     const usersCollection = db.collection("users");
     const ridersCollection = db.collection("riders");
 
-    app.post('/users',async(req,res)=>{
-      const user = req.body 
-      user.role = 'user'
-      user.createdAt = new Date()
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      user.role = "user";
+      user.createdAt = new Date();
 
-      const email = user.email 
-      const existUser = await usersCollection.findOne({email})
-      if(existUser){
-        return res.send({message:'user already exist'})
+      const email = user.email;
+      const existUser = await usersCollection.findOne({ email });
+      if (existUser) {
+        return res.send({ message: "user already exist" });
       }
-      const result = await usersCollection.insertOne(user)
-      res.send(result)
-    })
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     app.post("/parcels", async (req, res) => {
       const parcelData = req.body;
@@ -133,26 +130,29 @@ async function run() {
       res.send({ url: session.url });
     });
 
-    
-
     app.patch("/payment-success", async (req, res) => {
       const sessionId = req.query.session_id;
       const session = await stripe.checkout.sessions.retrieve(sessionId);
-      const transactionId = session.payment_intent
-      const query = {transactionId:transactionId}
-      const paymentExist = await paymentCollection.findOne(query)
-      if(paymentExist){
-        return res.send({message: 'aleardy exist',transactionId,trackingId:paymentExist.trackingId})
+      const transactionId = session.payment_intent;
+      const query = { transactionId: transactionId };
+      const paymentExist = await paymentCollection.findOne(query);
+      if (paymentExist) {
+        return res.send({
+          message: "aleardy exist",
+          transactionId,
+          trackingId: paymentExist.trackingId,
+        });
       }
       console.log(session);
       if (session.payment_status === "paid") {
         const id = session.metadata.parcelId;
         const query = { _id: new ObjectId(id) };
-        const trackingId = "TRK-" + Math.random().toString(36).substring(2, 10).toUpperCase();
+        const trackingId =
+          "TRK-" + Math.random().toString(36).substring(2, 10).toUpperCase();
         const update = {
           $set: {
             paymentStatus: "paid",
-            trackingId: trackingId
+            trackingId: trackingId,
           },
         };
         const result = await parcelsCollection.updateOne(query, update);
@@ -166,7 +166,7 @@ async function run() {
           transactionId: session.payment_intent,
           paymentStatus: session.payment_status,
           PaidAt: new Date(),
-          trackingId: trackingId
+          trackingId: trackingId,
         };
 
         if (session.payment_status === "paid") {
@@ -175,7 +175,7 @@ async function run() {
             success: true,
             modifyParcel: result,
             trackingId: trackingId,
-            transactionId:session.payment_intent,
+            transactionId: session.payment_intent,
             paymentInfo: paymentResult,
           });
         }
@@ -183,43 +183,70 @@ async function run() {
       res.send({ success: false });
     });
 
+    app.get("/payments", firebaseToken, async (req, res) => {
+      const email = req.query.email;
+      // console.log(req.headers)
+      const query = {};
+      if (email) {
+        query.customerEmail = email;
 
-    app.get('/payments',firebaseToken, async (req,res)=>{
-      const email = req.query.email
-      // console.log(req.headers) 
-      const query = {}
-      if(email){
-        query.customerEmail= email
-
-        // check the email 
-        if(email !== req.decoded_email){
-          return res.status(403).send({message: 'forbidden'})
+        // check the email
+        if (email !== req.decoded_email) {
+          return res.status(403).send({ message: "forbidden" });
         }
       }
-      const cursor =  paymentCollection.find(query)
-      const result =  await cursor.toArray()
-      res.send(result)
-    })
+      const cursor = paymentCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    // riders related api
+    app.post("/riders", async (req, res) => {
+      const rider = req.body;
+      rider.status = "pending";
+      rider.createdAt = new Date();
+      const result = await ridersCollection.insertOne(rider);
+      res.send(result);
+    });
 
-    // riders related api 
-    app.post('/riders',async(req,res)=>{
-      const rider = req.body 
-      rider.status = "pending"
-      rider.createdAt = new Date()
-      const result = await ridersCollection.insertOne(rider)
-      res.send(result)
-    })
-
-    app.get('/riders',async(req,res)=>{
-      const status = req.query.status 
-      const query = {}
-      if(status){
-        query.status = status
+    app.get("/riders", async (req, res) => {
+      const status = req.query.status;
+      const query = {};
+      if (status) {
+        query.status = status;
       }
-      const result = await ridersCollection.find(query).toArray()
-      res.send(result)
-    })
+      const result = await ridersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.patch("/riders/:id", async (req, res) => {
+      const status = req.body.status;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateInfo = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await ridersCollection.updateOne(query, updateInfo);
+
+      // update user role
+      if (status === "accepted") {
+        const email = req.body.email;
+        const userQuery = { email };
+        const updateUser = {
+          $set: {
+            role: "rider",
+          },
+        };
+        const userResult = await usersCollection.updateOne(
+          userQuery,
+          updateUser
+        );
+      }
+
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
